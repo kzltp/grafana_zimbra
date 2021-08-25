218980e8e7eef00e0159641e51483f5af8e9284f
Arif Kızıltepe <kzltpsgm@gmail.com>
2021-08-25 12:30:53 +0300

    Update checkzimbrastatus.sh

--- checkzimbrastatus.sh
+++ checkzimbrastatus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-status=$(cat /tmp/status.tmp | grep $1| grep -v Host | awk '{print $NF}')
+status=$(cat /tmp/status.tmp | grep $1| grep -v Host | awk 'NR==1{print $NF}')
 
 if [[ $status == Running ]]; 
 then
